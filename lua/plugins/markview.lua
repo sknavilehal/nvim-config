f0cf0223bda62579c47eacc25796ab398eb71a19
Shamanth <sknavilehal@gmail.com>
2026-06-23 13:24:15 +0200

    config update

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,8 +1,8 @@
 -- For `plugins/markview.lua` users.
 return {
-    "OXY2DEV/markview.nvim",
-    lazy = false,
-
-    -- Completion for `blink.cmp`
-    -- dependencies = { "saghen/blink.cmp" },
-};
+  "OXY2DEV/markview.nvim",
+  lazy = false,
+  ft = { "markdown"},
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {},
+}
