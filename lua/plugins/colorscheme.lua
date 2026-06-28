266c4d091c62f01eb2a541b71a62985f080317f0
Shamanth <sknavilehal@gmail.com>
2026-06-28 22:03:13 +0200

    updated coloscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,7 +2,17 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "unokai",
+      colorscheme = "sonokai",
     },
+  },
+
+  {
+    "sainnhe/sonokai",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.sonokai_style = "default"        -- try: atlantis, andromeda, shusia, maia
+      vim.g.sonokai_better_performance = 1
+    end,
   },
 }
